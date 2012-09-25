97e3ba72108b55b0cbadcb1e5983110637ededd9
chaow <chao.wei@aalto.fi>
2012-09-26 00:19:28 +0300

    adding examples for session 5

---
+++ WP71Demo/Model/ComputerList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace WP71Demo.Model
+{
+    [DataContract]
+    public class ComputerList
+    {
+        [DataMember(Name = "result")]
+        public string result { get; set; }
+
+        [DataMember(Name = "computers")]
+        public List<Computer> computerList { get; set; }
+
+    }
+
+}
